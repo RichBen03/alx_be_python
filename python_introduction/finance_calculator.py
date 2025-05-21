17dc26d8c3da53f7d261a45a48daf07ff1b53ef4
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-22 00:59:30 +0300

    Writing a python script to calculate the finances in a year for a user

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-monthly_income = input("Enter your total monthly income: ")
-monthly_expenses = input("Enter your total monthly expenses: ")
+monthly_income = input("Enter your total monthly income:")
+monthly_expenses = input("Enter your total monthly expenses:")
 monthly_savings = monthly_income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
