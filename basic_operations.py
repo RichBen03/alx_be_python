50c0e25fbff1bd49874e3b2c596c30e92d40fa73
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-21 23:59:33 +0300

    Basic arthimetic operations using python

--- basic_operations.py
+++ basic_operations.py
@@ -4,9 +4,8 @@ number2 = 5
 addition = number1 + number2
 subtraction = number1 - number2
 multiplication= number1 * number2
-division = number1 / number2
 
-print(f"Addition of 10 and 5 is {addition} ")
-print(f"Subtraction of10 and 5 is{subtraction} ")
-print(f"Multiplication of10 and 5 is {multiplication}")
-print(f"Division of 10 and 5 is {division}" )
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is{subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
