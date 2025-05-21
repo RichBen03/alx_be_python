75104d0d22e3f79b9b4da491e2d79639ff0f1127
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-21 23:55:26 +0300

    Performed basic operations in python

--- basic_operations.py
+++ basic_operations.py
@@ -6,7 +6,7 @@ subtraction = number1 - number2
 multiplication= number1 * number2
 division = number1 / number2
 
-print("Addition of10 and 5 is "+ addition)
-print("Subtraction of10 and 5 is"+ subtraction)
-print("Multiplication of10 and 5 is"+ multiplication)
-print("Division of10 and 5 is " + division)
+print(f"Addition of 10 and 5 is {addition} ")
+print(f"Subtraction of10 and 5 is{subtraction} ")
+print(f"Multiplication of10 and 5 is {multiplication}")
+print(f"Division of 10 and 5 is {division}" )
