294334624cf7cfb95e2e25c7bda36d01238db362
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-29 22:14:19 +0300

    Wrote a condition to check for a user's weather condition and provide recommendations using conditional statements

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+weather = str(input("How's the weather today? sunny/rainy/cold "))
+
+if weather == "sunny":
+    print("Wear a tshirt and some sunglasses")
+elif weather == "rainy":
+    print("Dont forget your umbrella and raincoat")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf")
+else:
+    print(f"Sorry i dont have recommendations to this weather {weather}")
+    
