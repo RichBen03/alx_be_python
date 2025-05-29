9f21fc0258d768844fb55f01a1db6db697e674bb
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-29 23:55:24 +0300

    Created a simple program that provided a reminder to user using condition statements

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,20 @@
+task = input("Enter a task description: ")
+priorty = input("What's the tasks priority?(high,medium,low) ")
+time_bound = input("Is the task time bound? (yes/no)")
+
+match priorty:
+    case "high":
+        if time_bound== "yes":
+                print(f"{task} is a high priority task that requires immediate attention today!")
+        else:
+             print(f"{task} is a high priority task find time and do it!")
+    case "medium":
+          if time_bound == "yes":
+               print(f"{task} is a medium priorty task but is time-bound please find time and complete it")
+          else:
+               print(f"{task} is a medium priority task, find time to do it ")
+    case "low":
+          if time_bound == "yes":
+               print(f"{task} is of low priority but kindly remember to do it on time, avoid procastination")
+          else:
+               print(f"{task} is a low priority task. Consider completing it when you have free time. ")
