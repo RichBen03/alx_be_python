ca12af5766daa0e427755691c1c939b582149a57
Rich Benjamin <richmwendwa03@gmail.com>
2025-05-29 23:29:45 +0300

    Created a square pattern that is generated by the size of the user's desire

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+pattern = int(input("Enter the size of the pattern: "))
+i=1
+while i <= pattern:
+    j=1
+    while j<=pattern:
+        print("*",end="")
+        j+=1
+    print()
+    i+=1
+   
+
